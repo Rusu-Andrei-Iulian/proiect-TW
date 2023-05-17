670d733a14eaa4ea2138177e99427b5277221d0b
Rusu Andrei_iulian <lord212003@gmail.com>
2023-05-18 00:58:15 +0300

    Finished Project

--- index.js
+++ index.js
@@ -1,10 +1,57 @@
 const cards = document.querySelectorAll('.card');
-let effect = document.getElementById("effect");
-const hero = document.getElementsByClassName('hero')[0];
 let images = ["images/Cartoon_Forest_BG_01.png", "images/Cartoon_Forest_BG_02.png"]
+
+addToSessionStorage();
+addToLocalStorage();
 
 setInterval(() => {
     let random = Math.floor(Math.random() * 2)
-    hero.style.backgroundImage = "url(" + images[random] + ")";
-    hero.style.transition = "background-image 1s"
+    $("#hero").css("backgroundImage", "url(" + images[random] + ")");
+    $("#hero").css("transition", "background-image 1s");
 }, 20000)
+
+function addToLocalStorage() {
+    const wizard = {
+        class: 'wizard',
+        magic: 'fire',
+        attacks: [{
+            name: "fire-ball",
+            damage: 29,
+            mana: 15,
+            efficientAgainst: ['ice'],
+            noEffectOn: ['earth']
+        }]
+    };
+    localStorage.setItem('wizardInfo', JSON.stringify(wizard));
+}
+
+function addToSessionStorage() {
+    const knight = {
+        class: 'knight',
+        weapon: 'sword',
+        attacks: [{
+            name: "slash",
+            damage: 18,
+            mana: 10,
+            efficientAgainst: ['humans', 'monsters'],
+            noEffectOn: ['ghost']
+        }]
+    };
+    sessionStorage.setItem('knightInfo', JSON.stringify(knight));
+}
+
+$(function () {
+    const ninja = {
+        class: 'ninja',
+        weapon: 'shuriken',
+        attacks: [{
+            name: "throw-shuriken",
+            damage: 16,
+            mana: 8,
+            efficientAgainst: ['humans', 'undergroud-monsters'],
+            noEffectOn: ['ghost']
+        }]
+    }
+
+    document.cookie = 'ninjaInfo=' + JSON.stringify(ninja);
+})
